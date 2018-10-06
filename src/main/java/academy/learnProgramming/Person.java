e5f73bee92c63b9158a6dca7b92b4e731d4220ef
venu2804 <venu2804@gmail.com>
2018-10-06 17:59:29 +0100

    First Commit

---
+++ src/main/java/academy/learnProgramming/Person.java
@@ -0,0 +1,19 @@
+package academy.learnProgramming;
+
+public class Person {
+    //Declaring variable
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    //define methods
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+
+        this.firstName=firstName;
+    }
+}
